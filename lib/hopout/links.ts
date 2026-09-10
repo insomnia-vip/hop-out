01e03e4e6e38d55b185137c797e5f1756277ef88
HOP OUT Builder <hop-out@users.noreply.github.com>
2026-09-10 23:23:24 +0300

    Point Twitter links to project profile

--- lib/hopout/links.ts
+++ lib/hopout/links.ts
@@ -1,5 +1,5 @@
 export const PROJECT_LINKS = {
   github: "https://github.com/insomnia-vip/hop-out",
-  twitter: "https://x.com/search?q=%24HOPOUT",
+  twitter: "https://x.com/insomnia_vip",
   pons: "https://www.ponsfamily.com/",
 } as const;
